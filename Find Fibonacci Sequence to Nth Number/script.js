858019853d57b16a331892b172abc9cb5d4d2b5c
Luke Orth <lorth001@gmail.com>
2018-06-22 05:56:27 -0400

    Update script.js

--- Find Fibonacci Sequence to Nth Number/script.js
+++ Find Fibonacci Sequence to Nth Number/script.js
@@ -1,27 +1,29 @@
-// initialize variables
-let Fn = 0;
-let Fn1 = 1;
-let Fn2 = 1;
-// initialize fib sequence and include first two seeds
-let fibArray = [1, 1];
-
-// function to find fib sequence
 function myFunction() {
+    // initialize variables
+    let Fn = 0;
+    let Fn1 = 1;
+    let Fn2 = 1;
+    // initialize fib sequence and include first two seeds
+    let fibArray = [1, 1];
     // initialize user-entered number
     let userNum = document.getElementById("length").value;
     
+    // make sure user enters positive number
     if (userNum < 1) {
         document.getElementById("showFib").innerHTML = "Please enter a positive number.";
     }
     
+    // if number is less than 2, display '1'
     else if (userNum < 2) {
         document.getElementById("showFib").innerHTML = 1;
     }
     
+    // if number is less than 3, display '1, 1'
     else if (userNum < 3) {
         document.getElementById("showFib").innerHTML = Fn1 + ", " + Fn2;
     }
     
+    // otherwise, find and display fib sequence
     else {
         for (let i = 0; i < (userNum - 2); i++) {
             Fn = Fn1 + Fn2; 
@@ -31,15 +33,4 @@ function myFunction() {
             document.getElementById("showFib").innerHTML = fibArray.join(', ');
         }
     }
-    // reset values
-    Fn = 0;
-    Fn1 = 1;
-    Fn2 = 1;
-    fibArray = [1, 1];
-}
-
-// function to clear fib sequence
-function clearFunction() {
-    document.getElementById("length").value = 0;
-    document.getElementById("showFib").innerHTML = "";
 }
