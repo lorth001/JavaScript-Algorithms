0757f406442dd27ba5a6a6be1337a4de41789ed3
Luke Orth <lorth001@gmail.com>
2018-06-29 07:37:50 -0400

    Added .addEventListener and comments

--- Numbers/4. Find Primes/script.js
+++ Numbers/4. Find Primes/script.js
@@ -1,27 +1,32 @@
-/* 
-Find Prime Numbers - Have the user enter a number and find/dispaly all Prime Numbers (if there are any) 
-leading up to the user's number.
+/*
+**********
+Luke Orth - Find Primes - 6/21/2018
+**********
+Last edit on 6/29/2018 -- replace .onClick with .addEventListener
+**********
+Have the user enter a number and find all Prime values leading up to declared number
+**********
 */
 
-// get number to test from user
+// create variable for getting number from user
 let userNum = document.getElementById("testNum").value;
+// create variable for event handler
+let findPrimes = document.getElementById("findPrimes");
 
 function myFunction() {
-    // initialize array to store prime numbers
     let primeArray = [];
-    // nested for loops to find prime numbers
     for (let i = 2; i <= userNum; i++) {
         for (let j = 2; j <= userNum; j++) {
             if (i === j) {
-                // if prime, then 'push' to array
                 primeArray.push(i);
             }
             if (i % j === 0) {
-                // break and repeat loop if not prime
                 break;
             }
         }
     }
-    // output prime numbers
     document.getElementById("showPrimes").innerHTML = primeArray.join(', ');
 }
+
+// execute myFunction when button is clicked
+findPrimes.addEventListener("click", myFunction);
