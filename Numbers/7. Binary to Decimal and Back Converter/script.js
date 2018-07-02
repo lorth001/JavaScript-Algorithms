a5a7ea11cf57d1f23227d2c859ac6cbd76589f49
Luke Orth <lorth001@gmail.com>
2018-07-01 22:17:51 -0400

    Added .addEventHandler and comments

--- Numbers/7. Binary to Decimal and Back Converter/script.js
+++ Numbers/7. Binary to Decimal and Back Converter/script.js
@@ -1,3 +1,18 @@
+/*
+**********
+Luke Orth - Binary to Decimal and Back Converter - 6/25/2018
+**********
+Last edit on 7/01/2018
+**********
+This program will take a user's input in either binary or decimal format and convert it to the opposite format
+**********
+*/
+
+// create variable for decimal event handler
+let getDec = document.getElementById("getDec");
+// create variable for binary event handler
+let getBin = document.getElementById("getBin");
+
 // Function to convert binary to decimal
 function getDecimal() {
     // Get binary number from user
@@ -21,3 +36,8 @@ function getBinary() {
     document.getElementById("showBinary").innerHTML = finalBin;
 	return finalBin;
 }
+
+// execute getDecimal when button is clicked
+getDec.addEventListener("click", getDecimal);
+// execute getBinary when button is clicked
+getBin.addEventListener("click", getBinary);
